2a5e68e3e6e7cfcb0152b616433ee9a424942bfc
Ali Koudri <ali.koudri@gmail.com>
2020-04-25 06:45:18 +0100

    First commit

---
+++ OwlGenerator.cs
@@ -0,0 +1,114 @@
+﻿using Express;
+using Hime.Redist;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Express_Parser
+{
+    class OwlGenerator: ExpressParser.Actions
+    {
+        private string baseName;
+        private TextWriter writer;
+        private bool debug = false;
+        private string[] primitiveTypes = { "NUMBER", "STRING" };
+
+        public OwlGenerator(string baseName, string fileName)
+        {
+            this.baseName = baseName;
+            if (debug) writer = Console.Out;
+            else writer = new StreamWriter(fileName);
+            writer.WriteLine("Prefix(owl:=<http://www.w3.org/2002/07/owl#>)");
+            writer.WriteLine("Prefix(rdf:=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>)");
+            writer.WriteLine("Prefix(rdfs:=<http://www.w3.org/2000/01/rdf-schema#>)");
+            writer.WriteLine("Prefix(xml:=<http://www.w3.org/XML/1998/namespace>)");
+            writer.WriteLine("Prefix(xsd:=<http://www.w3.org/2001/XMLSchema#>)");
+        }
+
+        public override void OnSchema(Symbol head, SemanticBody body)
+        {
+            string schema = body[0].Value;
+            writer.WriteLine($"Prefix(:=<{this.baseName}#>)\n");
+            writer.WriteLine($"Ontology(<{this.baseName}/{schema}>");
+        }
+
+        public override void OnUse(Symbol head, SemanticBody body)
+        {
+            //We assume that IRIs have the same base
+            writer.WriteLine($"Import (<{this.baseName}/{body[0].Value}>)");
+        }
+
+        public override void OnSelectType(Symbol head, SemanticBody body)
+        {
+            writer.WriteLine($"EquivalentClasses(:{body[0].Value} ObjectUnionOf(");
+            for (int i = 1; i < body.Length; i++)
+            {
+                writer.WriteLine($":{body[i].Value}");
+            }
+            writer.WriteLine("))");
+        }
+
+        public override void OnPrimitiveType(Symbol head, SemanticBody body)
+        {
+            writer.WriteLine($"DatatypeDefinition(:{body[0].Value} {this.GetPrimitiveType(body[1].Value)})");
+        }
+
+        public override void OnEntity(Symbol head, SemanticBody body)
+        {
+            writer.WriteLine($"Declaration(Class(:{body[0].Value}))");
+        }
+
+        public override void OnAttribute(Symbol head, SemanticBody body)
+        {
+            string name = body[0].Value;
+            string type;
+            bool optional = false;
+            if ("OPTIONAL".Equals(body[1].Value))
+            {
+                optional = true;
+                type = body[2].Value;
+            }
+            else
+            {
+                type = body[1].Value;
+            }
+            if (this.primitiveTypes.Contains(type))
+            {
+                this.ProcessPrimitiveAttribute(name, optional, type);
+                return;
+            }
+            this.ProcessReferenceAttribute(name, optional, type);
+        }
+
+        private void ProcessPrimitiveAttribute(string name, bool optional, string type)
+        {
+            writer.WriteLine($"DataPropertyDomain(:{name} :{this.GetPrimitiveType(type)})");
+            //FIXME: Retrieve parent node to get the range
+        }
+
+        private void ProcessReferenceAttribute(string name, bool optional, string type)
+        {
+            writer.WriteLine($"ObjectPropertyDomain(:{name} :{type})");
+            //FIXME: Retrieve parent node to get the range
+        }
+
+        public void Close()
+        {
+            writer.WriteLine(")");
+            writer.Close();
+        }
+
+        private string GetPrimitiveType(string key)
+        {
+            switch(key)
+            {
+                case "STRING":
+                    return "xsd:string";
+                case "NUMBER":
+                    return "xsd:int";
+                default:
+                    return "rdfs:Literal";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Hime.Redist;
+using System;
+using Express;
+using System.Text;
+using System.IO;
+
+namespace Express_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) return;
+            string fileName = args[1];
+            string text = File.ReadAllText(fileName);
+            OwlGenerator generator = new OwlGenerator(args[0], args[2]);
+            ExpressLexer lexer = new ExpressLexer(text);
+            ExpressParser parser = new ExpressParser(lexer, generator);
+            //parser.Parse();
+            ParseResult res = parser.Parse();
+            generator.Close();
+            //Print(res.Root, new bool[] { });
+            //Console.ReadKey();
+        }
+
+        private static void Print(ASTNode node, bool[] crossings)
+        {
+            for (int i = 0; i < crossings.Length - 1; i++)
+                Console.Write(crossings[i] ? "|   " : "    ");
+            if (crossings.Length > 0)
+                Console.Write("+-> ");
+            Console.WriteLine(node.ToString());
+            for (int i = 0; i != node.Children.Count; i++)
+            {
+                bool[] childCrossings = new bool[crossings.Length + 1];
+                Array.Copy(crossings, childCrossings, crossings.Length);
+                childCrossings[childCrossings.Length - 1] = (i < node.Children.Count - 1);
+                Print(node.Children[i], childCrossings);
+            }
+        }
+    }
+}
