7b9c9133c04dadf1e6ac1e99197c61e166d9f9c0
Ali Koudri <ali.koudri@gmail.com>
2020-07-13 16:28:21 +0100

    Fixed minor issues

--- Program.cs
+++ Program.cs
@@ -10,13 +10,23 @@ namespace Express_Parser
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0) return;
-            string fileName = args[1];
-            string text = File.ReadAllText(fileName);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: express2owl base_iri input_file [debug]");
+                return;
+            }
+            string inputFile = args[1];
+            string text = File.ReadAllText(inputFile);
             ExpressLexer lexer = new ExpressLexer(text);
             ExpressParser parser = new ExpressParser(lexer);
             ParseResult res = parser.Parse();
-            ExpressModelGenerator generator = new ExpressModelGenerator(res.Root, args[0], args[2], true);
+            string outputFile = inputFile.Replace(".exp", ".owl");
+            bool debug = false;
+            if (args.Length == 3)
+            {
+                debug = (args[2].Equals("debug")) ? true : false;
+            }
+            ExpressModelGenerator generator = new ExpressModelGenerator(res.Root, inputFile, outputFile, debug);
             generator.GenerateSchema();
             generator.GenerateOWl();
         }
